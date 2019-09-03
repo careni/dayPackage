a957044dc777e9cdec73fff64d1bc9a026814222
careni <526587423@qq.com>
2019-09-03 14:29:42 +0800

    cookie包修改

---
+++ 4-cookie/cookie.md
@@ -0,0 +1,45 @@
+# cookie字符串解析以及cookie对象的序列化
+
+## 来源
+express框架的依赖包
+
+## cookie讲解
+该包的实现比较简单，没有什么难点，在此就不再解析。
+
+  前端设置cookie的方式是通过document.cookie.cookie包含以下参数：
+
+  1.name
+  >需要设置的键名 username
+
+  2.value
+  >需要设置的键值 'ronnie'
+
+  3.Domian
+  >cookie所在的域名 'baidu.com'
+
+  4.Path
+  >cookie所访问的路径 '/'
+
+  5.Expires
+  >cookie的过期时间，时间戳'2027-10-21T07:48:25.783Z'
+
+  6.Max-Age
+  >cookie的有效时间，默认为-1（当次会话有效）以秒为单位 3600
+
+  7.Secure
+  >只有在https协议下，cookie才会被带在请求中发送到服务器
+
+  8.HttpOnly
+  >是否允许javascript操作cookie
+
+  9.SameSite
+  >是否允许携带cookie，有两个可选值Strict，Lax。Strict模式下任何请求都不允许携带cookie，Lax模式下一些安全的Http请求可以携带，例如<a>,<link>, <form get>这些，想ajax，post，<img>都是不可携带的
+
+## cookie的拼接
+根据以上的讲解我们可以拼接一个如下的字符串
+```javascript
+var ck = 'username=ronnie;domain=baidu.com;path=/;expires=2027-10-21T07:48:25.783Z;max-age=3600;secure;sameSite=Lax'
+
+document.cookie = ck;
+// 其中httponly需由服务端设置
+````
--- 4-cookie/index.js
+++ 4-cookie/index.js
@@ -1,1 +1,138 @@
 
+'use strict';
+
+exports.parse = parse;
+exports.serialize = serialize;
+
+var decode = decodeURIComponent;
+var encode = encodeURIComponent;
+var pairSplitRegExp = /; */;
+
+// 字符限制
+var fieldContentRegExp = /^[\u0009\u0020-\u007e\u0080-\u00ff]+$/;
+
+// 解析cookie
+function parse(str, options) {
+  if (typeof str !== 'string') {
+    throw new TypeError('argument str must be a string');
+  }
+
+  var obj = {}
+  var opt = options || {};
+  var pairs = str.split(pairSplitRegExp);
+  var dec = opt.decode || decode;
+
+  for (var i = 0; i < pairs.length; i++) {
+    var pair = pairs[i];
+    var eq_idx = pair.indexOf('=');
+
+    if (eq_idx < 0) {
+      continue;
+    }
+
+    var key = pair.substr(0, eq_idx).trim()
+    var val = pair.substr(++eq_idx, pair.length).trim();
+
+    if ('"' == val[0]) {
+      val = val.slice(1, -1);
+    }
+
+    if (undefined == obj[key]) {
+      obj[key] = tryDecode(val, dec);
+    }
+  }
+
+  return obj;
+}
+
+
+function serialize(name, val, options) {
+  var opt = options || {};
+  var enc = opt.encode || encode;
+
+  if (typeof enc !== 'function') {
+    throw new TypeError('option encode is invalid');
+  }
+
+  if (!fieldContentRegExp.test(name)) {
+    throw new TypeError('argument name is invalid');
+  }
+
+  var value = enc(val);
+
+  if (value && !fieldContentRegExp.test(value)) {
+    throw new TypeError('argument val is invalid');
+  }
+
+  var str = name + '=' + value;
+
+  if (null != opt.maxAge) {
+    var maxAge = opt.maxAge - 0;
+    if (isNaN(maxAge)) throw new Error('maxAge should be a Number');
+    str += '; Max-Age=' + Math.floor(maxAge);
+  }
+
+  if (opt.domain) {
+    if (!fieldContentRegExp.test(opt.domain)) {
+      throw new TypeError('option domain is invalid');
+    }
+
+    str += '; Domain=' + opt.domain;
+  }
+
+  if (opt.path) {
+    if (!fieldContentRegExp.test(opt.path)) {
+      throw new TypeError('option path is invalid');
+    }
+
+    str += '; Path=' + opt.path;
+  }
+
+  if (opt.expires) {
+    if (typeof opt.expires.toUTCString !== 'function') {
+      throw new TypeError('option expires is invalid');
+    }
+
+    str += '; Expires=' + opt.expires.toUTCString();
+  }
+
+  if (opt.httpOnly) {
+    str += '; HttpOnly';
+  }
+
+  if (opt.secure) {
+    str += '; Secure';
+  }
+
+  if (opt.sameSite) {
+    var sameSite = typeof opt.sameSite === 'string'
+      ? opt.sameSite.toLowerCase() : opt.sameSite;
+
+    switch (sameSite) {
+      case true:
+        str += '; SameSite=Strict';
+        break;
+      case 'lax':
+        str += '; SameSite=Lax';
+        break;
+      case 'strict':
+        str += '; SameSite=Strict';
+        break;
+      case 'none':
+        str += '; SameSite=None';
+        break;
+      default:
+        throw new TypeError('option sameSite is invalid');
+    }
+  }
+
+  return str;
+}
+
+function tryDecode(str, decode) {
+  try {
+    return decode(str);
+  } catch (e) {
+    return str;
+  }
+}
